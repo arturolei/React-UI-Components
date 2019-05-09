158cfdd1fa23b174714addd0115c1b319146ac64
arturolei <artie.lei@gmail.com>
2019-05-08 20:10:34 -0700

    Initial commit

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => {(
+  <ImageThumbnail />
+  <div>
+      <HeaderTitle/>
+      <HeaderContent>
+  </div>
+
+
+)};
+
+export default HeaderContainer;

caa3296a2b054b6211e8b0c2a453dc3f6b1733ae
arturolei <artie.lei@gmail.com>
2019-05-11 12:06:01 -0700

    Almost MVP complete for calculator; js is more or less where I want it

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import './App.css';
 
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
+
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="calc-container">
+      <CalculatorDisplay displayText="0"/>
+    
+      <div className="calc-button-block">
+        <div className="calc-white-block">
+          <ActionButton buttonClass="action-button-white" text="Clear"/>
+          <NumberButton buttonStyle="num" text="1"/>
+          <NumberButton buttonStyle="num" text="2"/>
+          <NumberButton buttonStyle="num" text="3"/>
+          <NumberButton buttonStyle="num" text="4"/>
+          <NumberButton buttonStyle="num" text="5"/>
+          <NumberButton buttonStyle="num" text="6"/>
+          <NumberButton buttonStyle="num" text="7"/>
+          <NumberButton buttonStyle="num" text="8"/>
+          <NumberButton buttonStyle="num" text="9"/>
+          <NumberButton buttonStyle="num-zero" text ="0"/>
+        </div>
+        <div className="action-button-col">
+          <ActionButton buttonClass="action-button" text="÷"/>
+          <ActionButton buttonClass="action-button" text="x"/>
+          <ActionButton buttonClass="action-button" text="-"/>
+          <ActionButton buttonClass="action-button" text="+"/>
+          <ActionButton buttonClass="action-button" text="="/>
+        </div>
+      </div>
+    
     </div>
   );
 };
